b926833aae9186e33d2192780634e4f35d33074e
Ashoka-001 <ashokkamdar7@gmail.com>
2024-03-15 03:54:36 +0530

    Added some styling

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ mongoose.connect(uri)
 .then(() => console.log('Database connected successfully'))
 .catch((err) => console.error('Database connection error: ', err));
 
-
+//movie schema to save the movie details in database
 const movieSchema = new mongoose.Schema({
   movieName : String,
   thumbnail : String,
@@ -28,13 +28,16 @@ const movieSchema = new mongoose.Schema({
   rating : String
 });
 
+//create the model for the schema to be used as json object
+
 const movieDB = mongoose.model("movieDB", movieSchema);
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-// connecting multer to upload a file(thumbnail)
+// use multer to add the poster (thumbnail) of the movie
+
 const storage = multer.diskStorage({
   destination: "public/uploads/",
   filename: (req, file, cb) => {
@@ -45,32 +48,17 @@ const storage = multer.diskStorage({
 });
 const upload = multer({ storage: storage });
 
-// home page
+// handle the get request for the landing page
 app.get("/", async function(req, res) {
-  // get all the movies 
+
+  // diplay all available movies in database
+
   const data = await movieDB.find();
   res.render("index", { data,count:""});  
-  // res.send(data);
 });
 
-// add Movie into database 
-app.post("/addMovie", upload.single("thumbnail"),async function(req, res){
-  const {movieName, director, language, releaseYear, rating } = req.body;
-  try{
-    const thumbnail = req.file.filename;
-    const newMovie = new movieDB({
-      movieName, director, language, releaseYear, rating, thumbnail
-    });
-    await newMovie.save();
-    res.redirect("/");
+// update the movie details if any correction
 
-  }
-  catch{
-    res.status(500).send("Could Not add the movie");
-  }
-});
-
-// update the movie
 app.post("/updateMovie", upload.single('thumbnail'),async function(req, res){
   const {_id ,movieName, director, language, releaseYear, rating } = req.body;
   try{
@@ -87,12 +75,33 @@ app.post("/updateMovie", upload.single('thumbnail'),async function(req, res){
 
   }
   catch{
-    res.status(500).send("Could Not update the movie");
+    res.status(500).send("Movie can not updated");
   }
 });
 
-// filter and search 
-app.get("/filterAndSearch/:param/:value", async function( req, res){
+// adding  Movie details into the database 
+
+app.post("/addMovie", upload.single("thumbnail"),async function(req, res){
+  const {movieName, director, language, releaseYear, rating } = req.body;
+  try{
+    const thumbnail = req.file.filename;
+    const newMovie = new movieDB({
+      movieName, director, language, releaseYear, rating, thumbnail
+    });
+    await newMovie.save();
+    res.redirect("/");
+
+  }
+  catch{
+    res.status(500).send("Error in adding movies");
+  }
+});
+
+
+
+// find and display the result acoording to filter used
+
+app.get("/findAnddisplay/:param/:value", async function( req, res){
   const parameter = req.params.param;
   const value = req.params.value;
   const query = {};
@@ -102,37 +111,41 @@ app.get("/filterAndSearch/:param/:value", async function( req, res){
   res.render("index", { data, count:"" });
 
 }); 
-//delete
-app.get("/delete/:id", async function(req, res){
-  try{
-    const id = req.params.id;
-    const deletedMovie = await movieDB.findOneAndDelete(id);
-    if(deletedMovie){
-      console.log("Deleted the movie");
-      res.redirect("/");
-    }
-    else{
-      res.send("Already Deleted the movie");
-    }
-  }
-  catch{
-    res.send("Couldn't delete the movie");
-  }
-});
-//count
+//counting number of movies based on language
+
 app.get("/countMovie/:lang", async function(req, res){
   try{
     const value = req.params.lang;
     const countMovies = await movieDB.countDocuments({language : value});
-    const msg = `There are a total of ${countMovies} movies in ${value} language.` ;
+    const msg = `Number of movies : ${countMovies} in ${value}.` ;
     const data = await movieDB.find();
     res.render("index",{data,count:msg});
   }
   catch{
     
-    res.send("Could not count the movies.");
+    res.send("unable to find movies.");
   }
 });
+
+//delete the details of action perfomed previously
+
+app.get("/delete/:id", async function(req, res){
+  try{
+    const id = req.params.id;
+    const deletedMovie = await movieDB.findOneAndDelete(id);
+    if(deletedMovie){
+      //console.log("Deleted the movie");
+      res.redirect("/");
+    }
+    else{
+      res.send("Details are not found");
+    }
+  }
+  catch{
+    res.send("can not delete the detail");
+  }
+});
+
    
 
 const  start = async() => {
