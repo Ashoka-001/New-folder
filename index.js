91eca1cd610f55532f8cad2e2a3f8ee45c6290c9
Ashoka-001 <ashokkamdar7@gmail.com>
2024-03-15 01:58:52 +0530

    envirnment variable issue

--- index.js
+++ index.js
@@ -137,7 +137,7 @@ app.get("/countMovie/:lang", async function(req, res){
 
 const  start = async() => {
   await mongoose.connect(uri).then (console.log("connetced db"));
-  app.listen(process.env.PORT, function() {
+  app.listen(3000, function() {
   
     console.log(`Server started on port ${process.env.PORT}`);
   });
